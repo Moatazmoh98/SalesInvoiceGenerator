bde8ae71840ae0c301e5d0f0ba9f04980449f8ab
Moatazmoh98 <moatazabdelzaher3@gmail.com>
2023-02-08 09:44:47 +0200

    Uplaod The Project

---
+++ SalesInvoiceGenerator/src/com/testingproject/model/InvoiceHeaderTableModel.java
@@ -0,0 +1,48 @@
+package com.testingproject.model;
+
+
+import com.testingproject.view.InvoiceFrame;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+public class InvoiceHeaderTableModel extends AbstractTableModel {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -1956022147958826252L;
+	private ArrayList<InvoiceHeader> invoicesArray;
+    private String[] columns = {"Invoice Num", "Invoice Date", "Customer Name", "Invoice Total"};
+    
+    public InvoiceHeaderTableModel(ArrayList<InvoiceHeader> invoicesArray) {
+        this.invoicesArray = invoicesArray;
+    }
+
+    @Override
+    public int getRowCount() {
+        return invoicesArray.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        InvoiceHeader inv = invoicesArray.get(rowIndex);
+        switch (columnIndex) {
+            case 0: return inv.getNum();
+            case 1: return InvoiceFrame.dateFormat.format(inv.getInvDate());
+            case 2: return inv.getCustomer();
+            case 3: return inv.getInvoiceTotal();
+        }
+        return "";
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columns[column];
+    }
+}
+
